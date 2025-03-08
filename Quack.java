06fba0f382739321f3e5f05c126453d58b50668e
Dhaerya <dhaeryamore@gmail.com>
2025-03-08 20:17:52 +0530

    Initial Commits

---
+++ Quack.java
@@ -0,0 +1,10 @@
+package Duck;
+
+public class Quack implements Quackbehaviour{
+
+    @Override
+    public void quack() {
+        // TODO Auto-generated method stub
+        System.out.println("Quack Quack");
+    }
+}
