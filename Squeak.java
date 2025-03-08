5a863905095a2f3891bbe88ad776a6651a3ba559
Dhaerya <dhaeryamore@gmail.com>
2025-03-08 20:19:24 +0530

    Initial Commits

---
+++ Squeak.java
@@ -0,0 +1,11 @@
+package Duck;
+
+public class Squeak implements Quackbehaviour{
+
+    @Override
+    public void quack() {
+        // TODO Auto-generated method stub
+       System.out.println("Squeekk'");
+    }
+    
+}
