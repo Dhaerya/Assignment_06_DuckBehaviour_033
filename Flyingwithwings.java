701ffbd70a2a2e6731fa0c25e9ba7b033a8e1472
Dhaerya <dhaeryamore@gmail.com>
2025-03-08 20:16:58 +0530

    Initial Commits

---
+++ Flyingwithwings.java
@@ -0,0 +1,11 @@
+package Duck;
+
+public class Flyingwithwings extends Duck{
+
+    @Override
+    public void display() {
+        // TODO Auto-generated method stub
+        System.out.println("I Can Fly");
+    }
+    
+}
