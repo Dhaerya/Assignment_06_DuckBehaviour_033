3b8a6111a525473e1ea22a2cabf712534a07ad46
Dhaerya <dhaeryamore@gmail.com>
2025-03-08 20:16:17 +0530

    Initial Commits

---
+++ Floating.java
@@ -0,0 +1,11 @@
+package Duck;
+
+public class Floating implements SwinBehaviour{
+
+    @Override
+    public void swim() {
+        // TODO Auto-generated method stub
+        System.out.println("I can float.....");
+    }
+    
+}
