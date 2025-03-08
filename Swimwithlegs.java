462ee5669358b9cd6e00770a8abb4b8490df390e
Dhaerya <dhaeryamore@gmail.com>
2025-03-08 20:19:59 +0530

    Initial Commits

---
+++ Swimwithlegs.java
@@ -0,0 +1,11 @@
+package Duck;
+
+public class Swimwithlegs implements SwinBehaviour{
+
+    @Override
+    public void swim() {
+        // TODO Auto-generated method stub
+        System.out.println("I belive I can Swim");
+    }
+    
+}
