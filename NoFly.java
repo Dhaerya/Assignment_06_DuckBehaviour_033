8e663a4cc7723da40d91c21fbbc80040340ff7fc
Dhaerya <dhaeryamore@gmail.com>
2025-03-08 20:17:42 +0530

    Initial Commits

---
+++ NoFly.java
@@ -0,0 +1,11 @@
+package Duck;
+
+public class NoFly extends Duck{
+
+    @Override
+    public void display() {
+        // TODO Auto-generated method stub
+        System.out.println("No Fly");
+    }
+    
+}
