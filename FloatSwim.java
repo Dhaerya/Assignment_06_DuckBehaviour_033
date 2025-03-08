f9b65d0818d9ada3583cf824d07f0afa025828e4
Dhaerya <dhaeryamore@gmail.com>
2025-03-08 21:16:09 +0530

    Code after changes and Implementation

---
+++ FloatSwim.java
@@ -0,0 +1,6 @@
+// FloatSwim.java  (renamed from Float.java)
+class FloatSwim implements SwimBehavior {
+    public void swim() {
+        System.out.println("This duck floats on water.");
+    }
+}
